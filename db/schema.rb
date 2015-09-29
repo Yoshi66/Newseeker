b7008820edd7f8b819376e3b0bfa90863aebd03f
Yoshiki <yoshiki.mc@gmail.com>
2015-09-28 22:10:21 -0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929043058) do
+ActiveRecord::Schema.define(version: 20150929044538) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.string   "content"
     t.string   "url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.string   "user_id"
+    t.string   "photo_file_name"
+    t.string   "photo_content_type"
+    t.integer  "photo_file_size"
+    t.datetime "photo_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
