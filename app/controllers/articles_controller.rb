db1d21348db802ee0b16e2cdeeba63f379e24533
Yoshiki <yoshiki.mc@gmail.com>
2015-09-28 22:08:04 -0700

    paperclip

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -69,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :content, :url)
+      params.require(:article).permit(:title, :content, :url,:photo)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
   belongs_to :user
+  has_attached_file :photo, styles: { medium: "300x300>", thumb: "100x100>" },
+  :url  => "/assets/arts/:id/:style/:basename.:extension", # 画像保存先のURL先
+  :path => "#{Rails.root}/public/assets/arts/:id/:style/:basename.:extension" # サーバ上の画像保存先パス
+  validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+    ## Validation
+  validates_attachment :photo,
+      presence: true,  # ファイルの存在チェック
+      less_than: 5.megabytes # ファイルサイズのチェック
 end
