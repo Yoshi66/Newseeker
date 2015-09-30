02d3a90788f8a23cd4397f2ba52309fde119012e
Yoshiki <yoshiki.mc@gmail.com>
2015-09-30 15:13:50 -0700

    fix design

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,7 +5,8 @@ class Article < ActiveRecord::Base
   has_attached_file :photo,:styles => {
                         :thumb  => "90x60",
                         :medium => "408x189",
-                        :large => "815x378"},
+                        :square => "100x100",
+                        :large => "815x378",},
   :url  => "/assets/arts/:id/:style/:basename.:extension", # 画像保存先のURL先
   :path => "#{Rails.root}/public/assets/arts/:id/:style/:basename.:extension" # サーバ上の画像保存先パス
   validates_attachment :photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
