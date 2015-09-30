2727c4752e07a14abd8546463df6383c9005ce4c
Yoshiki <yoshiki.mc@gmail.com>
2015-09-30 16:23:34 -0700

    add picture into user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  has_attached_file :profile_photo,:styles => {
+                        :thumb  => "90x60",
+                        :medium => "408x189",
+                        :square => "100x100",
+                        :large => "815x378",},
+  :url  => "/assets/arts/:id/:style/:basename.:extension", # 画像保存先のURL先
+  :path => "#{Rails.root}/public/assets/arts/:id/:style/:basename.:extension" # サーバ上の画像保存先パス
+  validates_attachment :profile_photo, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+  # ## Validation
+  validates_attachment :profile_photo,
+       presence: true,  # ファイルの存在チェック
+       less_than: 5.megabytes # ファイルサイズのチェック
 end
