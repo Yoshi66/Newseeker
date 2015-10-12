811149e72254e494f652307afb36e21e29fd0866
Yoshiki <yoshiki.mc@gmail.com>
2015-10-11 22:35:29 -0700

    search with title and content

--- app/models/article.rb
+++ app/models/article.rb
@@ -32,7 +32,7 @@ class Article < ActiveRecord::Base
 
   def self.search(query)
     # where(:title, query) -> This would return an exact match of the query
-    where("title like ?", "%#{query}%")
+    where("title like ? or content like ?", "%#{query}%", "%#{query}%")
   end
 
 
